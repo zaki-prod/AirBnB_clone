26b4066c0963d223561eecb3f944a71f9cb3a3ab
zaki-prod <zakiprod04@gmail.com>
2023-08-13 14:25:56 +0000

    update user

---
+++ models/user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Creates a user class"""
+from models.base_model import BaseModel
+
+
+class User(BaseMOdel):
+	"""This class manages user objects"""
+
+	email = ""
+	password = ""
+	first_name = ""
+	last_name = ""
